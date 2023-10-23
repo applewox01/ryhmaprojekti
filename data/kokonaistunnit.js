a1b849599dbd3c5e913d76201c3402010b0403a7
applewox01 <joensuuniko1@gmail.com>
2023-10-23 10:22:45 +0300

    data

---
+++ data/kokonaistunnit.js
@@ -0,0 +1,452 @@
+export const kokonaistunnit = [
+	{
+	pvm: "2022-10-03",
+	nimi: "Tanja",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-03",
+	nimi: "Ben",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-03",
+	nimi: "Sahar",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-03",
+	nimi: "Mikko",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-03",
+	nimi: "Lisa",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-03",
+	nimi: "Jon",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-03",
+	nimi: "Rami",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-03",
+	nimi: "Veera",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-03",
+	nimi: "Jaiden",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-04",
+	nimi: "Tanja",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-04",
+	nimi: "Ben",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-04",
+	nimi: "Sahar",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-04",
+	nimi: "Mikko",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-04",
+	nimi: "Lisa",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-04",
+	nimi: "Jon",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-04",
+	nimi: "Rami",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-04",
+	nimi: "Veera",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-04",
+	nimi: "Jaiden",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-05",
+	nimi: "Tanja",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-05",
+	nimi: "Ben",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-05",
+	nimi: "Sahar",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-05",
+	nimi: "Mikko",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-05",
+	nimi: "Lisa",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-05",
+	nimi: "Jon",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-05",
+	nimi: "Rami",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-05",
+	nimi: "Veera",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-05",
+	nimi: "Jaiden",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-06",
+	nimi: "Tanja",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-06",
+	nimi: "Ben",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-06",
+	nimi: "Sahar",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-06",
+	nimi: "Mikko",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-06",
+	nimi: "Lisa",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-06",
+	nimi: "Jon",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-06",
+	nimi: "Rami",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-06",
+	nimi: "Veera",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-06",
+	nimi: "Jaiden",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-07",
+	nimi: "Tanja",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-07",
+	nimi: "Ben",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-07",
+	nimi: "Sahar",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-07",
+	nimi: "Mikko",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-07",
+	nimi: "Lisa",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-07",
+	nimi: "Jon",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-07",
+	nimi: "Rami",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-07",
+	nimi: "Veera",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-07",
+	nimi: "Jaiden",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-10",
+	nimi: "Tanja",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-10",
+	nimi: "Ben",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-10",
+	nimi: "Sahar",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-10",
+	nimi: "Mikko",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-10",
+	nimi: "Lisa",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-10",
+	nimi: "Jon",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-10",
+	nimi: "Rami",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-10",
+	nimi: "Veera",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-10",
+	nimi: "Jaiden",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-11",
+	nimi: "Tanja",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-11",
+	nimi: "Ben",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-11",
+	nimi: "Sahar",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-11",
+	nimi: "Mikko",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-11",
+	nimi: "Lisa",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-11",
+	nimi: "Jon",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-11",
+	nimi: "Rami",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-11",
+	nimi: "Veera",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-11",
+	nimi: "Jaiden",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-12",
+	nimi: "Tanja",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-12",
+	nimi: "Ben",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-12",
+	nimi: "Sahar",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-12",
+	nimi: "Mikko",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-12",
+	nimi: "Lisa",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-12",
+	nimi: "Jon",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-12",
+	nimi: "Rami",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-12",
+	nimi: "Veera",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-12",
+	nimi: "Jaiden",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-13",
+	nimi: "Tanja",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-13",
+	nimi: "Ben",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-13",
+	nimi: "Sahar",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-13",
+	nimi: "Mikko",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-13",
+	nimi: "Lisa",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-13",
+	nimi: "Jon",
+	tunteja: 8
+	},
+	{
+	pvm: "2022-10-13",
+	nimi: "Rami",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-13",
+	nimi: "Veera",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-13",
+	nimi: "Jaiden",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-14",
+	nimi: "Tanja",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-14",
+	nimi: "Ben",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-14",
+	nimi: "Sahar",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-14",
+	nimi: "Mikko",
+	tunteja: 5
+	},
+	{
+	pvm: "2022-10-14",
+	nimi: "Lisa",
+	tunteja: 7
+	},
+	{
+	pvm: "2022-10-14",
+	nimi: "Jon",
+	tunteja: 10
+	},
+	{
+	pvm: "2022-10-14",
+	nimi: "Rami",
+	tunteja: 6
+	},
+	{
+	pvm: "2022-10-14",
+	nimi: "Veera",
+	tunteja: 9
+	},
+	{
+	pvm: "2022-10-14",
+	nimi: "Jaiden",
+	tunteja: 6
+	}
+];
---
+++ data/tiedot.js
@@ -0,0 +1,67 @@
+export const tiimi1 = {
+	nimi: "Kotisivuprojekti";
+	jasenet: [
+		{
+			nimi: "Jaiden",
+			rooli: "graafikko, valokuvaaja",
+			osaaminen: "Valo- ja videokuvaus, Photoshop",
+			kuvaus: "Valokuvaaja, aviomies, isä ja innokas retkeilijä."
+		},
+		{
+			nimi: "Jon",
+			rooli: "Ohjelmoija",
+			osaaminen: "HTML5/CSS3, Angular, PHP, MySQL, Linux-palvelimet",
+			kuvaus: "Koodia 20 vuoden kokemuksella."
+		},
+		{
+			nimi: "Lisa",
+			rooli: "UX-suunnittelija",
+			osaaminen: "UI/UX Design, HTML5/CSS3, Photoshop, Illustrator",
+			kuvaus: "Käyttökokemuksen ja käyttöliittymien suunnittelun tuleva asiantuntija."
+		},
+		{
+			nimi: "Rami",
+			rooli: "Ohjelmoija",
+			osaaminen: "HTML5/CSS3, Javascript Full Stack",
+			kuvaus: "No comment."
+		},
+		{
+			nimi: "Veera",
+			rooli: "Projektipäällikkö, asiakasvastaava",
+			osaaminen: "Projektijohtaminen, Javascript, React, Redux",
+			kuvaus: "Asiakas voi olla samaan aikaan kuningas ja idiootti."
+		}
+	]
+};
+
+export const tiimi2 = 
+	nimi: "Mobiilisovellusprojekti";
+	jasenet: [
+		{
+			nimi: "Ben",
+			rooli: "Projektipäällikkö, graafikko",
+			osaaminen: "Kuvankäsittely, vektorigrafiikka, projektijohtaminen",
+			kuvaus: "I <3 video games."
+		},
+		{
+			nimi: "Mikko",
+			rooli: "UX-suunnittelija, ohjelmoija",
+			osaaminen: "UI/UX Design, HTML5/CSS3, Javascript, Kotlin",
+			kuvaus: "Hyvin suunniteltu on puoliksi tehty."
+		},
+		{
+			nimi: "Sahar",
+			rooli: "Ohjelmoija",
+			osaaminen: "C/C++, Java, Kotlin, mobiiliohjelmointi (Android / iOS)",
+			kuvaus: "Mimmit koodaa!"
+		},
+		{
+			nimi: "Tanja",
+			rooli: "Ohjelmoija, ohjelmistotestaaja",
+			osaaminen: "Mobiiliohjelmointi, DevOps",
+			kuvaus: "Ei spagettia, kiitos."
+		}
+	]
+};
+
+export const tyolajit = ["Määrittely", "Suunnittelu", "Toteutus", "Testaus", "Asiakastapaamiset"];
