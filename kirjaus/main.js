95db4a250f0031b7b6ac6effa6b8611e50ad1109
omena <joensuuniko1@gmail.com>
2023-10-24 11:10:39 +0300

    teema jne.

--- kirjaus/main.js
+++ kirjaus/main.js
@@ -3,8 +3,23 @@
 document.addEventListener("DOMContentLoaded", function(){
     let uusi = document.getElementById("uusi");
     let cloneDefault = document.getElementById("cloneDefault");
+
+    let kirjausLista = document.getElementById("kirjausLista");
+
+    let removeParent = function() {
+
+    };
+
     uusi.addEventListener("click",function(){
-        
+        let clone = cloneDefault.cloneNode(true);
+        kirjausLista.appendChild(clone);
+        let button = document.createElement("button");
+        button.innerHTML = "Poista";
+        button.addEventListener("click", removeParent);
+        button.className = "poistaButton";
+        clone.appendChild(button);
+
     });
+
 
 });
