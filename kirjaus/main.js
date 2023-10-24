f58c81cfdcf0214fc79d16a3b30ea76fcd98d8de
omena <joensuuniko1@gmail.com>
2023-10-24 10:24:01 +0300

    style tiedoston tulee olla kansion sisällä

---
+++ kirjaus/main.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    let uusi = document.getElementById("uusi");
+    let cloneDefault = document.getElementById("cloneDefault");
+    uusi.addEventListener("click",function(){
+        
+    });
+
+});
