58b6057f4dc60af06aa86fc59e846ad4297ef2cf
Atoku <atokufuseini@gmail.com>
2023-11-01 11:50:34 +0200

    tiimit

--- tiimit/tiimit.js
+++ tiimit/tiimit.js
@@ -1,7 +1,38 @@
+import { tiimi1, tiimi2 } from "../data/tiedot.js";
+
+function nayttatiimilaiset() {
+  document.getElementById("tiimilaiset1").innerHTML=""
+    let tiiminimi =  document.querySelector("#tiimival option:checked").value;
+    let tiimi;
+    if (tiiminimi =="tiimi1"){
+        tiimi=tiimi1
+    }
+  else if (tiiminimi=="tiimi2"){
+    tiimi=tiimi2
+  }
+
+  for (let jasen of tiimi.jasenet) {
+    const laatikko = document.createElement("div");
+    
+    const teksti = document.createTextNode(jasen.nimi + " " + jasen.rooli);
+    laatikko.appendChild(teksti);
+    document.getElementById("tiimilaiset1").appendChild(laatikko);
+    laatikko.addEventListener("click", function() {
+      nayttainfo(jasen)
+    })
+
+  }
+}
 
 
+document.getElementById("tiimival").addEventListener("change", nayttatiimilaiset);
 
 
+function nayttainfo(jasen){
+document.getElementById("info").innerHTML = `${jasen.nimi} <br>`
+document.getElementById("info").innerHTML += `${jasen.rooli} <br>`
+document.getElementById("info").innerHTML += `${jasen.osaaminen} <br>`
+document.getElementById("info").innerHTML += `${jasen.kuvaus} <br>`
 
 
-document.getElementById("info").innerHTML = info.tiedot();
+}
