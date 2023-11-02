6d41744817f031508bb3151c343477b08bcac601
Andrei <andrei.tsizikov00@gmail.com>
2023-11-02 10:40:41 +0200

    c

--- data/tiedot.js
+++ data/tiedot.js
@@ -53,7 +53,7 @@ export const tiimi2 = {
 			nimi: "Sahar",
 			rooli: "Ohjelmoija",
 			osaaminen: "C/C++, Java, Kotlin, mobiiliohjelmointi (Android / iOS)",
-			kuvaus: "pela koko paivä casino 666"
+			kuvaus: "pela koko paivä casino 666 ja lisäksi Ohjelmistokehittäjä ei osaa ja ei tykkä mutta hän pitä ohjelmoida erillaisia kakka ohjelmia, ei hyödyntää rajapintoja, ei tykkä käsitellä tietoa ja sen lisääksi ei osa käyttää versionhallintaa (esimerkiksi GitHub ja) lisääksi ohjelmojja tarjoa ilmaiseksi piratoitetuja pelejä ilmaiseksi (itorrents-igruha.org) nettisivulla . Työ aikana ohjelmistokehitäjä ei ohjelmoi mitään vaan piratooi elokuvia ja tietokone pelejä. Ohjelmistokehitystiimin kanssa ohjelmoja ei koskaan ei puhu mistään asioista . Asiakkaan kanssa ohjelmoja on tosi ärsytävä ja agresiivinen ja sano tosi huonoja juttuja asiakalle, yritä hujata ja varasta raha asiakan taskusta puhelun aikana. Ei suunnittelee ohjelmiston toteutuksen ja ei varmistaa toteutettavien toimintojen laadun."
 		},
 		{
 			nimi: "Tanja",
